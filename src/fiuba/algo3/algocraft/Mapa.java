e842bccaa5f90f26c3834754725557bcbe8d870b
MalenaMaffei <malenamaffei@hotmail.com>
2015-06-01 13:23:23 -0300

    matriz cambiada por hashmap de coordenadas, metodos actualizados

--- src/fiuba/algo3/algocraft/Mapa.java
+++ src/fiuba/algo3/algocraft/Mapa.java
@@ -4,30 +4,34 @@ public class Mapa {
 	private String nombre;
     private int columnas;
     private int filas;
-    private Parcela [][] matrizDeParcelas;
+    HashMap<Coordenada, Parcela> tablero; 
 
     
     public Mapa(String nombre, int columnas, int filas) {
         this.nombre = nombre;
         this.columnas = columnas;
         this.filas = filas;
-        this.matrizDeParcelas = new Parcela [columnas][filas];
+        this.tablero = new HashMap<List, Parcela>();
         this.llenarMapaConParcelasDeTierra();
     }
 
     private void llenarMapaConParcelasDeTierra() {
     	for (int fil = 0; fil < this.filas; fil++)
     	     for (int col = 0; col < this.columnas; col++)
-    	    	 this.matrizDeParcelas[col][fil] = new ParcelaTierra();
+    	    	 Coordenada coordenada = new Coordenada(x,y);
+    			 Parcela parcela = new ParcelaTierra();
+    	    	 this.tablero.put(coordenada,parcela);
     }
     
     public void ubicarElementoEnParcela(int x, int y, Interactuable elemento) throws ExcepcionElementoInvalidoParaParcela {
-    	Parcela parcela = this.matrizDeParcelas[x][y];
+    	Coordenada coord = new Coordenada(x,y);
+    	Parcela parcela = this.tablero.get(coord);
     	parcela.guardarElemento(elemento);
     }
     
     public Interactuable devolverElementoEnParcela(int x, int y) {
-    	Parcela parcela = this.matrizDeParcelas[x][y];
+    	Coordenada coord = new Coordenada(x,y);
+    	Parcela parcela = this.tablero.get(coord);
 
     	return parcela.devolverElemento();
     }
