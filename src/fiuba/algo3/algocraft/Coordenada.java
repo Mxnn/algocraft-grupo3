3f58b0d7b5f71b7b3beee2060711b25927c022b1
MalenaMaffei <malenamaffei@hotmail.com>
2015-06-01 13:24:52 -0300

    Clase coordenada para el hashmap del mapa,  equas y haschode overriden para que funcionen bien en el hashmap -> dos coordenadas son iguales si son iguales x e y

---
+++ src/fiuba/algo3/algocraft/Coordenada.java
@@ -0,0 +1,40 @@
+package fiuba.algo3.algocraft;
+public class Coordenada {
+	private int x;
+	private int y;
+	
+	public Coordenada(int cX, int cY){
+		this.x = cX;
+		this.y = cY;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+    @Override
+    public boolean equals(Object otro) 
+    {
+        if (this == otro)
+          return true;
+
+        if (!(otro instanceof Coordenada))
+          return false;
+
+        Coordenada otraCoor = (Coordenada) otro;
+        return otraCoor.getX() == this.x && otraCoor.getY() == this.y;
+    }
+	
+	@Override
+	public int hashCode() {
+	    int hash = 7;
+	    hash = 71 * hash + this.x;
+	    hash = 71 * hash + this.y;
+	    return hash;
+	}
+
+}
