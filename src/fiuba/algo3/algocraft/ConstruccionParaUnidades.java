6d3314b1bc1d532b754f5b87c058dbaf3cabf73f
MalenaMaffei <malenamaffei@hotmail.com>
2015-06-02 15:45:43 -0300

    implementan guardarEn parcela para pruebas

--- src/fiuba/algo3/algocraft/ConstruccionParaUnidades.java
+++ src/fiuba/algo3/algocraft/ConstruccionParaUnidades.java
@@ -1,5 +1,7 @@
 package fiuba.algo3.algocraft;
 
 public abstract class ConstruccionParaUnidades extends Construccion {
-
+	public void guardarEnParcela(Parcela parcela) throws ExcepcionElementoNoAdmitidoEnParcela {
+        parcela.guardarElemento(this);
+    }
 }
