38e602277b7427f831f78bbd156e7e53899b0963
MalenaMaffei <malenamaffei@hotmail.com>
2015-06-02 15:45:07 -0300

    puse que implemente interactuable para poder probar parcelas

--- src/fiuba/algo3/algocraft/Construccion.java
+++ src/fiuba/algo3/algocraft/Construccion.java
@@ -1,8 +1,10 @@
 package fiuba.algo3.algocraft;
 
-public abstract class Construccion { //Tiene que implementar interactuable y las hijas deben redefinir metodos
+public abstract class Construccion implements Interactuable{ //Tiene que implementar interactuable y las hijas deben redefinir metodos
 
 	protected Jugador propietario;
 	protected int vida;
 	protected int turnosDeCreacion;
+	
+	
 }
