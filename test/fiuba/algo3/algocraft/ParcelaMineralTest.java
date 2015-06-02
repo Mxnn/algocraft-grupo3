9c7790919be61427539deac9ac7fdd2d74f01b21
MalenaMaffei <malenamaffei@hotmail.com>
2015-06-02 16:19:50 -0300

    tests comentados a revisar, subo para dejar constancia

--- test/fiuba/algo3/algocraft/ParcelaMineralTest.java
+++ test/fiuba/algo3/algocraft/ParcelaMineralTest.java
@@ -3,6 +3,8 @@ package fiuba.algo3.algocraft;
 import fiuba.algo3.algocraft.Excepciones.ExcepcionElementoNoAdmitidoEnParcela;
 import fiuba.algo3.algocraft.RazaTerran.Espectro;
 import fiuba.algo3.algocraft.RazaTerran.Marine;
+import org.junit.Assert;
+
 import org.junit.Test;
 
 public class ParcelaMineralTest {
@@ -21,4 +23,27 @@ public class ParcelaMineralTest {
 
         parcela.guardarElemento(unidad);
     }
+//    @Test
+//    public void guardarExtractorMineralGuardaCorrectamente() throws ExcepcionElementoNoAdmitidoEnParcela{
+//    	Parcela parcela = new ParcelaMineral();
+//    	Interactuable extractorMineral = new CentroDeMineral();
+//    	parcela.guardarElemento(extractorMineral);
+//    	Assert.assertSame(parcela.devolverElemento(),extractorMineral);
+//    }
+    
+//    @Test(expected = ExcepcionElementoNoAdmitidoEnParcela.class)
+//    public void guardarElementoLanzaExcepcionSiElElementoEsUnExtractorDeGas() throws ExcepcionElementoNoAdmitidoEnParcela {
+//        Parcela parcela = new ParcelaMineral();
+//        Interactuable construccion = new Refineria(1,1);
+//
+//        parcela.guardarElemento(construccion);
+//    }
+    
+//    @Test(expected = ExcepcionElementoNoAdmitidoEnParcela.class)
+//    public void guardarElementoLanzaExcepcionSiElElementoEsUnaConstruccionParaUnidades() throws ExcepcionElementoNoAdmitidoEnParcela {
+//        Parcela parcela = new ParcelaMineral();
+//        Interactuable construccion = new Barraca();
+//
+//        parcela.guardarElemento(construccion);
+//    }
 }
--- test/fiuba/algo3/algocraft/ParcelaVolcanTest.java
+++ test/fiuba/algo3/algocraft/ParcelaVolcanTest.java
@@ -4,6 +4,7 @@ import fiuba.algo3.algocraft.Excepciones.ExcepcionElementoNoAdmitidoEnParcela;
 import fiuba.algo3.algocraft.RazaTerran.Espectro;
 import fiuba.algo3.algocraft.RazaTerran.Marine;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class ParcelaVolcanTest {
     @Test(expected = ExcepcionElementoNoAdmitidoEnParcela.class)
@@ -21,4 +22,28 @@ public class ParcelaVolcanTest {
 
         parcela.guardarElemento(unidad);
     }
+    
+//  @Test
+//  public void guardarExtractorGasGuardaCorrectamente() throws ExcepcionElementoNoAdmitidoEnParcela{
+//  		Parcela parcela = new ParcelaVolcan();
+//  		Interactuable extractor = new Refineria(1,1);
+//  		parcela.guardarElemento(extractor);
+//  		Assert.assertSame(parcela.devolverElemento(),extractor);
+//  }
+  
+//  @Test(expected = ExcepcionElementoNoAdmitidoEnParcela.class)
+//  public void guardarElementoLanzaExcepcionSiElElementoEsUnExtractorDeMineral() throws ExcepcionElementoNoAdmitidoEnParcela {
+//      Parcela parcela = new ParcelaVolcan();
+//      Interactuable extractor = new CentroDeMineral();
+//
+//      parcela.guardarElemento(extractor);
+//  }
+  
+//  @Test(expected = ExcepcionElementoNoAdmitidoEnParcela.class)
+//  public void guardarElementoLanzaExcepcionSiElElementoEsUnaConstruccionParaUnidades() throws ExcepcionElementoNoAdmitidoEnParcela {
+//      Parcela parcela = new ParcelaVolcan();
+//      Interactuable construccion = new Barraca();
+//
+//      parcela.guardarElemento(construccion);
+//  }
 }
