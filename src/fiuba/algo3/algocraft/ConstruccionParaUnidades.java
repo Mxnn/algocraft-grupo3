b263f920d0a3227f993ec7d893cb5244266cdd62
MalenaMaffei <malenamaffei@hotmail.com>
2015-06-07 22:33:34 -0300

    Delete ConstruccionParaUnidades.java
    ConstruccionParaUnidades borrada, ya no sirve

--- src/fiuba/algo3/algocraft/ConstruccionParaUnidades.java
+++
@@ -1,8 +0,0 @@
-package fiuba.algo3.algocraft;
-
-import fiuba.algo3.algocraft.Excepciones.ExcepcionElementoNoAdmitidoEnParcela;
-
-public abstract class ConstruccionParaUnidades extends Construccion {
-	//creo que esta la podriamos borrar sin problemas
-	
-}
