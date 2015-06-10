9b53e94aa8139e8c5096e23a2ec28bd17418ec60
MalenaMaffei <malenamaffei@hotmail.com>
2015-06-10 03:22:11 -0300

    mas tests para clon

--- test/fiuba/algo3/algocraft/ClonTest.java
+++ test/fiuba/algo3/algocraft/ClonTest.java
@@ -6,12 +6,15 @@ import org.junit.Test;
 
 import fiuba.algo3.algocraft.Excepciones.ExcepcionCoordenadaFueraDelMapa;
 import fiuba.algo3.algocraft.Excepciones.ExcepcionElementoNoAdmitidoEnParcela;
+import fiuba.algo3.algocraft.Excepciones.ExcepcionEnemigoFueraDeAlcance;
+import fiuba.algo3.algocraft.Excepciones.ExcepcionEnemigoNoAtacable;
 import fiuba.algo3.algocraft.Excepciones.ExcepcionEstadoMuerto;
 import fiuba.algo3.algocraft.Excepciones.ExcepcionNoHaySuministrosDisponibles;
 import fiuba.algo3.algocraft.Excepciones.ExcepcionNumeroDeBasesInvalido;
 import fiuba.algo3.algocraft.Excepciones.ExcepcionParcelaOcupada;
 import fiuba.algo3.algocraft.Excepciones.ExcepcionRecursosInsuficientes;
 import fiuba.algo3.algocraft.RazaProtoss.Dragon;
+import fiuba.algo3.algocraft.RazaTerran.Marine;
 
 public class ClonTest {
 
@@ -28,7 +31,7 @@ public class ClonTest {
 		
 		assertEquals(unJugador.cantidadDeUnidades(),1); //se espera una porque se murio el clon epro el dragon sigue
 	}
-	
+	@Test
 	public void clonTieneEscudoCorrecto() throws ExcepcionEstadoMuerto, ExcepcionNoHaySuministrosDisponibles, ExcepcionRecursosInsuficientes, ExcepcionCoordenadaFueraDelMapa, ExcepcionParcelaOcupada, ExcepcionElementoNoAdmitidoEnParcela, ExcepcionNumeroDeBasesInvalido{
 		Jugador unJugador = new Jugador("Juan", Color.ROJO, Protoss.getInstance());
 		Mapa mapa = new Mapa(2, 6, 6);
@@ -37,8 +40,9 @@ public class ClonTest {
 		
 		UnidadAgresora clon = new Clon(dragon, unJugador);
 		
-		assertEquals(80,((EstadoProtoss) clon.obtenerEstado()).obtenerEscudo()); //se espera una porque se murio el clon epro el dragon sigue
+		assertEquals(80,((EstadoProtoss) clon.obtenerEstado()).obtenerEscudo()); 
 	}
+	@Test
 	public void clonTieneCeroVida() throws ExcepcionEstadoMuerto, ExcepcionNoHaySuministrosDisponibles, ExcepcionRecursosInsuficientes, ExcepcionCoordenadaFueraDelMapa, ExcepcionParcelaOcupada, ExcepcionElementoNoAdmitidoEnParcela, ExcepcionNumeroDeBasesInvalido{
 		Jugador unJugador = new Jugador("Juan", Color.ROJO, Protoss.getInstance());
 		Mapa mapa = new Mapa(2, 6, 6);
@@ -47,7 +51,29 @@ public class ClonTest {
 		
 		UnidadAgresora clon = new Clon(dragon, unJugador);
 		
-		assertEquals(0,clon.obtenerEstado().obtenerVida()); //se espera una porque se murio el clon epro el dragon sigue
+		assertEquals(0,clon.obtenerEstado().obtenerVida()); 
+	}
+	
+	@Test
+	public void elDanyoDelAtaqueEsCero() throws ExcepcionEstadoMuerto, ExcepcionNoHaySuministrosDisponibles, ExcepcionRecursosInsuficientes, ExcepcionCoordenadaFueraDelMapa, ExcepcionParcelaOcupada, ExcepcionElementoNoAdmitidoEnParcela, ExcepcionNumeroDeBasesInvalido, ExcepcionEnemigoNoAtacable, ExcepcionEnemigoFueraDeAlcance{
+		Mapa mapa = new Mapa(2, 6, 6);
+		Jugador unJugador = new Jugador("Juan", Color.ROJO, Protoss.getInstance());
+	    unJugador.crearAdicionalDeSuministro(mapa, new Coordenada(3, 3));
+	    Jugador unJugador2 = new Jugador("Juan2", Color.VERDE, Terran.getInstance());
+	    unJugador2.crearAdicionalDeSuministro(mapa, new Coordenada(1, 3));
+	    
+		UnidadAgresora dragon = new Dragon(unJugador);
+		mapa.ubicarElementoEnParcela(new Coordenada(0,0), dragon);
+		
+		UnidadAgresora clon = new Clon(dragon, unJugador);
+		mapa.ubicarElementoEnParcela(new Coordenada(0,1), clon);
+		
+		Interactuable marine = new Marine(unJugador2);
+		mapa.ubicarElementoEnParcela(new Coordenada(1,0), marine);
+		
+		clon.atacar(marine);
+		
+		assertEquals(marine.obtenerVida(),40);
 	}
 	
 	
