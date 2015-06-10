1a5ca1c931669d58aaedf8b753b8aed81f657245
MalenaMaffei <malenamaffei@hotmail.com>
2015-06-10 01:09:31 -0300

    Update NexoMineralTest.java
    cambiado nombre metodo

--- test/fiuba/algo3/algocraft/NexoMineralTest.java
+++ test/fiuba/algo3/algocraft/NexoMineralTest.java
@@ -13,7 +13,7 @@ public class NexoMineralTest {
         Jugador unJugadorPropietario = new Jugador("Juan", Color.ROJO, Protoss.getInstance());
         ExtractorMineral nexoMineral = new NexoMineral(unJugadorPropietario);
 
-        nexoMineral.recolectarMinerales();
+        nexoMineral.collectarRecursos();
 
         Assert.assertEquals(unJugadorPropietario.obtenerMineral(), Jugador.MINERAL_INICIAL + 10);
     }
