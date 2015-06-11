bc8d702207bc99bf0b256b7ac50872d08a7b4bd8
Mxnn <Mxnn@users.noreply.github.com>
2015-06-11 14:15:42 -0300

    Eliminada la interfaz Alucinable

--- src/fiuba/algo3/algocraft/Alucinable.java
+++
@@ -1,9 +0,0 @@
-package fiuba.algo3.algocraft;
-
-import fiuba.algo3.algocraft.Excepciones.ExcepcionNoHaySuministrosDisponibles;
-
-public interface Alucinable {
-    public Alucinable crearAlucinacion() throws ExcepcionNoHaySuministrosDisponibles, CloneNotSupportedException;
-
-    public Jugador obtenerPropietario();
-}
