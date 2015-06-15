3bd2a2d6ac156435e9a8bad0a2afa18ac7c25be5
MalenaMaffei <malenamaffei@hotmail.com>
2015-06-15 00:14:02 -0300

    test nuevo para nave ciencia

--- test/fiuba/algo3/algocraft/NaveCienciaTest.java
+++ test/fiuba/algo3/algocraft/NaveCienciaTest.java
@@ -204,29 +204,45 @@ public class NaveCienciaTest {
 	     assertEquals(0, (nave2.getEnergia()));
 	}
 	
-//	@Test
-//	public void quitaEnergiaARadioConUnidadesMagicas() throws ExcepcionNombreCorto, ExcepcionNumeroDeBasesInvalido, ExcepcionRecursosInsuficientes, ExcepcionCoordenadaFueraDelMapa, ExcepcionParcelaOcupada, ExcepcionElementoNoAdmitidoEnParcela, ExcepcionNoHaySuministrosDisponibles, ExcepcionEstadoMuerto, ExcepcionEnemigoFueraDeAlcance, ExcepcionEnergiaInsuficiente {
-//		Jugador unJugador = new Jugador("Juan", Color.ROJO, Protoss.getInstance());
-//		 Jugador unJugador2 = new Jugador("Juan2", Color.VERDE, Terran.getInstance());
-//		 Mapa mapa = new Mapa(2, 5, 5);
-//	     unJugador.crearAdicionalDeSuministro(mapa, new Coordenada(3, 3));
-//	     unJugador2.crearAdicionalDeSuministro(mapa, new Coordenada(3, 1));
-//	     
-//	     UnidadMagica templario = new AltoTemplario(unJugador);
-//	     mapa.ubicarElementoEnParcela(new Coordenada (1,1), templario);
-//	     NaveCiencia naveCiencia = new NaveCiencia(unJugador2);
-//	     
-//	     
-//	     for (int i = 0; i < 6; i++) 
-//	    	 naveCiencia.regenerarEnergia();
-//	     
-//	     ArrayList<Parcela> listaParcelas = new ArrayList<Parcela>();
-//	     listaParcelas.add(mapa.obtenerParcelaEnCoordenada(new Coordenada (1,1)));
-//	     
-//	     Ataque ataque = naveCiencia.lanzarEMP(listaParcelas);
-//	     ataque.ejecutarAtaque();
-//	     
-//	     assertEquals(0, (templario.getEnergia()));
-//	}
+	@Test
+	public void quitaEnergiaARadioConUnidadesMagicas() throws ExcepcionNombreCorto, ExcepcionNumeroDeBasesInvalido, ExcepcionRecursosInsuficientes, ExcepcionCoordenadaFueraDelMapa, ExcepcionParcelaOcupada, ExcepcionElementoNoAdmitidoEnParcela, ExcepcionNoHaySuministrosDisponibles, ExcepcionEstadoMuerto, ExcepcionEnemigoFueraDeAlcance, ExcepcionEnergiaInsuficiente {
+		Jugador unJugador = new Jugador("Juan", Color.ROJO, Protoss.getInstance());
+		 Jugador unJugador2 = new Jugador("Juan2", Color.VERDE, Terran.getInstance());
+		 Mapa mapa = new Mapa(2, 5, 5);
+	     unJugador.crearAdicionalDeSuministro(mapa, new Coordenada(3, 3));
+	     unJugador.crearAdicionalDeSuministro(mapa, new Coordenada(4, 4));
+	     unJugador2.crearAdicionalDeSuministro(mapa, new Coordenada(3, 1));
+	     
+	     UnidadMagica templario1 = new AltoTemplario(unJugador);
+	     UnidadMagica templario2 = new AltoTemplario(unJugador);
+	     UnidadMagica templario3 = new AltoTemplario(unJugador);
+	     UnidadMagica templario4 = new AltoTemplario(unJugador);
+	     mapa.ubicarElementoEnParcela(new Coordenada (1,1), templario1);
+	     mapa.ubicarElementoEnParcela(new Coordenada (1,2), templario2);
+	     mapa.ubicarElementoEnParcela(new Coordenada (1,3), templario3);
+	     mapa.ubicarElementoEnParcela(new Coordenada (2,1), templario4);
+	     NaveCiencia naveCiencia = new NaveCiencia(unJugador2);
+	     
+	     ArrayList<Parcela> listaParcelas = new ArrayList<Parcela>();
+	     listaParcelas.add(templario1.getParcela());
+	     listaParcelas.add(templario2.getParcela());
+	     listaParcelas.add(templario3.getParcela());
+	     listaParcelas.add(templario4.getParcela());
+	     
+	     for (int i = 0; i < 6; i++) 
+	    	 naveCiencia.regenerarEnergia();
+	     
+	     Ataque ataque = naveCiencia.lanzarEMP(listaParcelas);
+	     ataque.ejecutarAtaque();
+	     
+	     boolean energiaEsCero;
+	     
+	     energiaEsCero = (0 == (templario1.getEnergia()));
+	     energiaEsCero = (0 == (templario2.getEnergia()));
+	     energiaEsCero = (0 == (templario3.getEnergia()));
+	     energiaEsCero = (0 == (templario4.getEnergia()));
+	     
+	     assertTrue(energiaEsCero);
+	}
 
 }
