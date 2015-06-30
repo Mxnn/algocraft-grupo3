454e4634b0cc150be2c512cbeeb91738c2976adb
Matias <matias.larrart@gmail.com>
2015-06-30 13:15:32 -0300

    arreglando mover mensaje

---
+++ src/fiuba/algo3/algocraft/modelo/excepciones/ExcepcionUnidadParaDeMover.java
@@ -0,0 +1,7 @@
+package fiuba.algo3.algocraft.modelo.excepciones;
+
+public class ExcepcionUnidadParaDeMover extends Exception {
+	public ExcepcionUnidadParaDeMover(){
+		super("Unidad para de mover");
+	}
+}
